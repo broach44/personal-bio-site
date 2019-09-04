e6893f61a7e49bbe655d1093b15f63e14038e5c0
Crystal Broach <crysland@gmail.com>
2019-09-03 23:48:46 -0500

    starting to line out functions and js structure

--- projects.js
+++ projects.js
@@ -1,4 +1,38 @@
+
+
+const projects = [
+    {
+    title: 'Cool Project',
+    screenshot: 'http://gotoflashgames.com/files/file/033.jpg',
+    description: 'This is the best project', // A good project description includes 'the what', 'the why', and 'the how'.
+    technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: true,
+    url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+    githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
+    },
+    {
+    title: 'Cool Project',
+    screenshot: 'http://gotoflashgames.com/files/file/033.jpg',
+    description: 'This is the best project', // A good project description includes 'the what', 'the why', and 'the how'.
+    technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: true,
+    url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+    githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
+    },
+
+]
+
 const printToDom = (divId, toPrint) => {
     document.getElementById(divId).innerHTML = toPrint;
 };
 
+//function to loop through the projects array and build domString
+const createProjectCards = () => {
+
+//add logic to only show the project on the page if it has a value of true in the available property
+
+//print to div called projectsPage
+
+}
+
+//Call the createProjectCards(); function in an init function that runs when the project loads.
